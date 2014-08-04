179ad9773190866708f3bd5d46361d36921dc46f
Sebastian Bachmann <me@free-minds.net>
2014-08-04 20:30:21 +0200

    Change the script and adopt the feedrate to a in real life tested value.

--- README.md
+++ README.md
@@ -12,3 +12,21 @@ In my setup i use:
 * outline: layer 3
 * component side: layer 1
 * everything else: layer 2
+
+Move `Makefile.sample` to your project as `Makefile`, adopt the target pcb name and then call
+`make`.
+Make sure `pcb2metalab.sh` is in your `$PATH`.
+
+Some remarks to pcb2gcode:
+--------------------------
+
+* It will cut on z=0, so you have to adjust the z touchoff
+  on your own! (like z=+0.035)
+* Only the drillfile uses the preamble/postamble! 
+  the other files will generate their own.
+  Because of this you have to manually start the spindle 
+  before drilling and milling!!!
+* We cut, mill and drill with the same millhead.
+  This is fine as long as we use a very small one. 0.5 or 0.6
+  mm are very good. 0.3mm are better for milling but they are
+  too short to mill through the whole pcb.
--- pcb2metalab.sh
+++ pcb2metalab.sh
@@ -4,42 +4,59 @@
 ##########################CONFIGURATION########################
 pcb2gcode=/usr/bin/pcb2gcode
 
-# some pcb2gcode versions need a preamble and postamble file, please check if these files work for you
-preamble=preamble.ngc
-postamble=postamble.ngc
+# get the scripts directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# all values are in mm
+# some pcb2gcode versions need a preamble and postamble file, please check if these files work for you
+# in the 1.1.4 version only the drillfile needs this!
+preamble=$DIR/preamble.ngc
+postamble=$DIR/postamble.ngc
+
+# the diameter of the millhead in mm
 millhead="0.5"
+
+# a higher offset generates less cuts, less offset generates an "exact" image of
+# the pcb. minimum should be $millhead
+offset=$millhead
+
 # choose some mm more than your pcb has. normal pcbs has 1.6mm
-pcb_thickness=1.8
-#feedrates mm per minute
-# TODO need to check if these values are correct...
-drillfeedrate=250
-cutfeedrate=400
-millfeedrate=400
+pcb_thickness=1.7
+
+# tested 250mm/min cuting and milling, which seems quite good with a 0.5mm
+# millhead.
+# you should not go higher because the 0.5mm mill will break!
+
+# feedrates mm per minute
+feedrate=250
+
+# how many mm we should cat in one z change.
+# used when cuting and drilling.
+infeed=`awk "BEGIN {print 0.4*$millhead;}"`
+
 # speed 22000rpm should be fine
 speed=22000
-#height to move around
-safe=4
-offset=$millhead
+
+# height to move around
+safe=10
+
 #################################################################
 
 PCB=$1
 brdname=$(basename $PCB .pcb)
 
-infeed=`awk "BEGIN {print 0.4*$millhead;}"`
 
 
 OPTS=""
 OPTS+="--back ${brdname}.bottom.gbr "
+OPTS+="--top ${brdname}.bottom.gbr "
 OPTS+="--outline ${brdname}.outline.gbr "
 OPTS+="--drill ${brdname}.plated-drill.cnc "
 
 OPTS+="--metric --milldrill  "
 OPTS+="--zwork 0 --zsafe $safe --zchange 10 --zcut -$pcb_thickness --cutter-diameter $millhead "
-OPTS+="--zdrill -$infeed --drill-feed $drillfeedrate --drill-speed $speed "
+OPTS+="--zdrill -$infeed --drill-feed $feedrate --drill-speed $speed "
 OPTS+="--offset $offset "
-OPTS+="--mill-feed $millfeedrate --mill-speed $speed --cut-feed $cutfeedrate --cut-speed $speed --cut-infeed $infeed "
+OPTS+="--mill-feed $feedrate --mill-speed $speed --cut-feed $feedrate --cut-speed $speed --cut-infeed $infeed "
 OPTS+="--dpi 1200 "
 OPTS+="--basename $brdname "
 OPTS+="--preamble $preamble --postamble $postamble "
